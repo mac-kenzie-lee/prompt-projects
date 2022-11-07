fc169d0cef5627e0e6812fabd7b4cfd6e34e8d51
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-06 23:48:52 -0800

    added error for zero division

--- logic.js
+++ logic.js
@@ -1,34 +1,6 @@
 
 
 const calculator = {};
-// calculator.divide = (x,y)=> {
-//     return x / y;
-// },
-// calculator.multiply = (x,y) => {
-//     return x * y;
-// },
-// calculator.add = (x,y) => {
-//     return x + y;
-// },
-// calculator.subtract = (x,y) => {
-//     return x - y;
-// },
-// calculator.percentage = (x,y) => {
-//     return x * (1 + y/100);  
-// },
-// calculator.operate = (operator, num1, num2)=>  {
-//     if (operator === '+') {
-//        return calculator.add(num1, num2)
-//     } else if (operator === '-') {
-//        return calculator.subtract(num1, num2)
-//     } else if (operator === '*') {
-//        return calculator.multiply(num1, num2)
-//     } else if (operator === '/') {
-//        return calculator.divide(num1, num2)
-//     } else if (operator === '%') {
-//        return calculator.percentage(num1, num2)
-//     }
-//}
 
 
 //Selectors------------------------------------------------
@@ -37,7 +9,7 @@ const screenDisplay = document.querySelector(".calculator-screen-text");
 const numberButtonsArray = document.querySelectorAll(".number");
 const clearButton =  document.querySelector("#clear")
 const operatorButtonArray = document.querySelectorAll(".operator");
-
+const deci = document.querySelector('#decimal')
 //event listeners ----------------------------------------------------
 clearButton.addEventListener('click', clearDisplay) 
 clearButton.addEventListener('dblclick', masterClear) 
@@ -48,6 +20,7 @@ turnOnTheOperators()
 
 calculator.defaultDisplay = true;
 calculator.toDisplay = "";
+refreshDeci()
 
 //functions ---------------------------------------------------------
 function turnOnTheOperators() {   
@@ -61,9 +34,23 @@ function turnOffTheOperators() {
     }
 }
 
+function refreshDeci() {
+    deci.addEventListener('click', updateDeci)
+    calculator.deciUsed = false;
+}
 
+function updateDeci(e){
+    
+    if (calculator.deciUsed === false) {
+        calculator.deciUsed = true;
+        updateDisplay(e)
+        deci.removeEventListener('click', updateDeci)
+    }
+
+}
 
 function updateDisplay(e) {
+    
     calculator.defaultDisplay = false;
     if (calculator.toDisplay.length > 15) {
         return null;
@@ -81,22 +68,29 @@ function clearDisplay() {
     calculator.toDisplay = ""
     console.clear()
     turnOnTheOperators()
+    refreshDeci()
 }
 
 function masterClear() {
     // delete calculator.expression
-     console.log('all clear?')
      delete calculator.num1;
      delete calculator.num2;
+     delete calculator.operator
      delete calculator.summation
-  //   console.log(calculator.expression)
+     console.clear()
+    refreshDeci()
+     //   console.log(calculator.expression)
   //   calculator.expression = [];
      screenDisplay.textContent = 'memory cleared'
  }
 
 const add = (n, n2) => n + n2
 const subtract = (n, n2) => n - n2
-const divide = (n, n2) => n / n2
+const divide = (n, n2) => {
+    if (n === 0 || n2 === 0) {
+        return 'ERROR'
+    }
+    return n / n2}
 const multiply = (n, n2) => n * n2
 
 const operate = (op, num1, num2) => {
@@ -117,15 +111,14 @@ const operate = (op, num1, num2) => {
         return sum;
     }
 
+    const per = document.querySelector('#per')
 function saveOperatorVariable(e) {
     calculator.operator = e.target.parentElement.children[0].innerText;
     if (calculator.num1 === undefined) {
         calculator.num1 = Number(screenDisplay.textContent)
     } 
     clearDisplay();
-    console.log(calculator.operator)
-    console.log(calculator.num1)
-
+    per.addEventListener('click', getThePercent)
     //removes other operator event listeners..
     turnOffTheOperators()
 
@@ -137,111 +130,30 @@ equalButton.addEventListener('click', findSolution)
 
 
 function findSolution() {
-    console.log('xd')
-
-    console.log(calculator.defaultDisplay)
+    //gets the sum after pressing equal sign
     if (calculator.defaultDisplay === false) {
-        console.log('n1' + calculator.num1)
         calculator.num2 = calculator.currentDisplay
-        console.log('n2' + calculator.num2)
-    
         calculator.summation = operate(calculator.operator, calculator.num1, calculator.num2)
         screenDisplay.textContent = calculator.summation;    
         turnOnTheOperators()
         calculator.num1 = calculator.summation    
-     //   delete calculator.num2
-       // calculator.num1 = calculator.summation
+        delete calculator.num2
     }
     
 }
 
+function getThePercent(){
+    if (calculator.operator !== undefined && calculator.num1 !== undefined && calculator.defaultDisplay === false) {
+        
+        calculator.num2 = calculator.currentDisplay;
+        if (calculator.operator === "+" || calculator.operator === "-") {
+            calculator.num2 = ((calculator.num1) * calculator.num2/100);
+        } else {
+            calculator.num2 = (1 + (calculator.num2/100))
+        }
 
+        calculator.summation = operate(calculator.operator, calculator.num1, calculator.num2)
+        screenDisplay.textContent = calculator.summation;
 
-
-
-
-
-
-
-
-
-
-
-
-
-//calculator.expression = [];
-
-
-//calculator.usedAnOperator = false;
-
-
-
-
-
-
-
-
-// function callAnOperator(e) {
-    
-   
-//     calculator.currentOperator = e.target.parentElement.children[0].innerText;
-//     calculator.cNum = (Number(screenDisplay.textContent));
-
-//     if (calculator.defaultDisplay === false) {
-//         //only proceeds if the user inputted a number
-
-
-//         screenDisplay.textContent = calculator.currSum
-//         }
-
-//     }
- 
-
-
-// function equate(e) {
-//     let solution;
-//     console.log('em')
-//     function evaluateIt(key, value, accumulator){
-//         if (key === "×") {
-//             return calculator.multiply(accumulator, value)
-//         }
-//         else if (key === "×÷"){
-//            return calculator.divide(accumulator, value)
-//         }
-//         else if (key === "−×÷") {
-//           return  calculator.subtract(accumulator, value)
-//         }
-//         else if (key === "+−×÷"){
-//             console.log(accumulator, value, key)
-//             return calculator.add(accumulator, value)
-//         }
-//     }
-
-
-//     if (calculator.usedAnOperator === true && calculator.defaultDisplay === false) {
-//         //checks if an operator was used once, and if another number was clicked
-//         let run = 0;
-//         calculator.lastExpression = (Number(screenDisplay.textContent))
-//         let sum = null;
-//         for (let element = 0; element < calculator.expression.length; element++) {
-//             //first run
-//             if (element === 0) {
-//                 //do the calculation setup for the first number.
-//                 sum = calculator.expression[element]
-                
-//             } else {
-//                 //not the first iteration
-//                 //loop just once
-//                 for (let key in (calculator.expression[element])) {
-//                     let value = calculator.expression[element][key]
-//                     sum = evaluateIt(key, value, sum)
-
-//                 }
-//             }
-
-//             sum = evaluateIt(calculator.currentOperator, calculator.lastExpression,sum)
-            
-//             return sum;
-//         }
-    
-//     }}
+    }
+} 

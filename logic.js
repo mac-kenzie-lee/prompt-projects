5cc7971f4ba2e2b3c90a7986769c81b5aa875a18
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-06 23:16:31 -0800

    Changed algorithm to take and evaluate two digits at a time

--- logic.js
+++ logic.js
@@ -1,61 +1,67 @@
 
 
 const calculator = {};
+// calculator.divide = (x,y)=> {
+//     return x / y;
+// },
+// calculator.multiply = (x,y) => {
+//     return x * y;
+// },
+// calculator.add = (x,y) => {
+//     return x + y;
+// },
+// calculator.subtract = (x,y) => {
+//     return x - y;
+// },
+// calculator.percentage = (x,y) => {
+//     return x * (1 + y/100);  
+// },
+// calculator.operate = (operator, num1, num2)=>  {
+//     if (operator === '+') {
+//        return calculator.add(num1, num2)
+//     } else if (operator === '-') {
+//        return calculator.subtract(num1, num2)
+//     } else if (operator === '*') {
+//        return calculator.multiply(num1, num2)
+//     } else if (operator === '/') {
+//        return calculator.divide(num1, num2)
+//     } else if (operator === '%') {
+//        return calculator.percentage(num1, num2)
+//     }
+//}
 
-calculator.divide = (x,y)=> {
-    return x / y;
-},
-calculator.multiply = (x,y) => {
-    return x * y;
-},
-calculator.add = (x,y) => {
-    return x + y;
-},
-calculator.subtract = (x,y) => {
-    return x - y;
-},
-calculator.percentage = (x,y) => {
-    return x * (1 + y/100);  
-},
-calculator.operate = (operator, num1, num2)=>  {
-    if (operator === '+') {
-       return calculator.add(num1, num2)
-    } else if (operator === '-') {
-       return calculator.subtract(num1, num2)
-    } else if (operator === '*') {
-       return calculator.multiply(num1, num2)
-    } else if (operator === '/') {
-       return calculator.divide(num1, num2)
-    } else if (operator === '%') {
-       return calculator.percentage(num1, num2)
-    }
-}
 
+//Selectors------------------------------------------------
 const equalButton = document.querySelector("#equal")
 const screenDisplay = document.querySelector(".calculator-screen-text");
-
 const numberButtonsArray = document.querySelectorAll(".number");
 const clearButton =  document.querySelector("#clear")
+const operatorButtonArray = document.querySelectorAll(".operator");
 
-//clears the display
+//event listeners ----------------------------------------------------
 clearButton.addEventListener('click', clearDisplay) 
-
-
 clearButton.addEventListener('dblclick', masterClear) 
-equalButton.addEventListener('click', equateAndShow)
-
-calculator.defaultDisplay = true;
-
 for (let btn of numberButtonsArray) {
     btn.addEventListener('click', updateDisplay)
 }
+turnOnTheOperators()
 
-const operatorButtonArray = document.querySelectorAll(".operator");
+calculator.defaultDisplay = true;
+calculator.toDisplay = "";
+
+//functions ---------------------------------------------------------
+function turnOnTheOperators() {   
 for (let op of operatorButtonArray) {
-    op.addEventListener('click', callAnOperator)
+    op.addEventListener('click', saveOperatorVariable)
+ }
+}
+function turnOffTheOperators() {
+    for (let op of operatorButtonArray) {
+        op.removeEventListener('click', saveOperatorVariable)
+    }
 }
 
-calculator.toDisplay = "";
+
 
 function updateDisplay(e) {
     calculator.defaultDisplay = false;
@@ -66,100 +72,176 @@ function updateDisplay(e) {
     //console.dir(e.target.parentElement.children[0].innerText)
     calculator.toDisplay += e.target.parentElement.children[0].innerText;
     screenDisplay.textContent = calculator.toDisplay;
+    calculator.currentDisplay = Number(calculator.toDisplay)
 }
-
-function equateAndShow(e){
-    calculator.toDisplay += e.target.parentElement.children[0].innerText;
-    screenDisplay.textContent = equate(e)
-
-}
-
-calculator.expression = [];
-
 //sets the display to be 0.
 function clearDisplay() {
     calculator.defaultDisplay = true;
     screenDisplay.textContent = "0.";
     calculator.toDisplay = ""
+    console.clear()
+    turnOnTheOperators()
 }
 
 function masterClear() {
-    delete calculator.expression
-    console.log('all clear?')
-    console.log(calculator.expression)
-    calculator.expression = [];
-    screenDisplay.textContent = 'memory cleared'
+    // delete calculator.expression
+     console.log('all clear?')
+     delete calculator.num1;
+     delete calculator.num2;
+     delete calculator.summation
+  //   console.log(calculator.expression)
+  //   calculator.expression = [];
+     screenDisplay.textContent = 'memory cleared'
+ }
+
+const add = (n, n2) => n + n2
+const subtract = (n, n2) => n - n2
+const divide = (n, n2) => n / n2
+const multiply = (n, n2) => n * n2
+
+const operate = (op, num1, num2) => {
+    let sum = null;
+    if (op ==='+') {
+        sum = add(num1, num2)    
+    }
+     else if (op === '−'){
+            sum = subtract(num1, num2)
+     }
+      else if (op === '×') {
+            sum = multiply(num1, num2)
+       }
+        else if (op === '÷') {
+            sum = divide(num1, num2)
+         }
+          console.log(sum)
+        return sum;
+    }
+
+function saveOperatorVariable(e) {
+    calculator.operator = e.target.parentElement.children[0].innerText;
+    if (calculator.num1 === undefined) {
+        calculator.num1 = Number(screenDisplay.textContent)
+    } 
+    clearDisplay();
+    console.log(calculator.operator)
+    console.log(calculator.num1)
+
+    //removes other operator event listeners..
+    turnOffTheOperators()
+
+
 }
-calculator.usedAnOperator = false;
 
-function callAnOperator(e) {
-    calculator.usedAnOperator = true;
-    calculator.currentOperator = e.target.parentElement.children[0].innerText;
+
+equalButton.addEventListener('click', findSolution)
+
+
+function findSolution() {
+    console.log('xd')
+
+    console.log(calculator.defaultDisplay)
     if (calculator.defaultDisplay === false) {
-        //only proceeds if the user inputted a number
-        if (calculator.expression.length === 0) {
-        calculator.firstNumber = Number(calculator.toDisplay);
-        calculator.nextOperator = calculator.currentOperator
-        calculator.expression.push(Number(calculator.toDisplay));
-        calculator.nextOperator = calculator.currentOperator;
+        console.log('n1' + calculator.num1)
+        calculator.num2 = calculator.currentDisplay
+        console.log('n2' + calculator.num2)
+    
+        calculator.summation = operate(calculator.operator, calculator.num1, calculator.num2)
+        screenDisplay.textContent = calculator.summation;    
+        turnOnTheOperators()
+        calculator.num1 = calculator.summation    
+     //   delete calculator.num2
+       // calculator.num1 = calculator.summation
     }
-        
-     
-        else {
-            console.log('erm')
-            calculator.expression.push({[calculator.nextOperator]:Number(calculator.toDisplay)});         
-            calculator.nextOperator = calculator.currentOperator;
-        }
+    
+}
 
-        clearDisplay()
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//calculator.expression = [];
+
+
+//calculator.usedAnOperator = false;
+
+
+
+
+
+
+
+
+// function callAnOperator(e) {
+    
+   
+//     calculator.currentOperator = e.target.parentElement.children[0].innerText;
+//     calculator.cNum = (Number(screenDisplay.textContent));
+
+//     if (calculator.defaultDisplay === false) {
+//         //only proceeds if the user inputted a number
+
+
+//         screenDisplay.textContent = calculator.currSum
+//         }
+
+//     }
  
 
-}
-function equate(e) {
-    let solution;
-    console.log('em')
-    function evaluateIt(key, value, accumulator){
-        if (key === "×") {
-            return calculator.multiply(accumulator, value)
-        }
-        else if (key === "÷"){
-           return calculator.divide(accumulator, value)
-        }
-        else if (key === "−") {
-          return  calculator.subtract(accumulator, value)
-        }
-        else if (key === "+"){
-            console.log(accumulator, value, key)
-            return calculator.add(accumulator, value)
-        }
-    }
+
+// function equate(e) {
+//     let solution;
+//     console.log('em')
+//     function evaluateIt(key, value, accumulator){
+//         if (key === "×") {
+//             return calculator.multiply(accumulator, value)
+//         }
+//         else if (key === "×÷"){
+//            return calculator.divide(accumulator, value)
+//         }
+//         else if (key === "−×÷") {
+//           return  calculator.subtract(accumulator, value)
+//         }
+//         else if (key === "+−×÷"){
+//             console.log(accumulator, value, key)
+//             return calculator.add(accumulator, value)
+//         }
+//     }
 
 
-    console.log('taco')
-    if (calculator.usedAnOperator === true && calculator.defaultDisplay === false) {
-        //checks if an operator was used once, and if another number was clicked
-        let run = 0;
-        calculator.lastExpression = (Number(screenDisplay.textContent))
-        let sum = null;
-        for (let element = 0; element < calculator.expression.length; element++) {
-            //first run
-            if (element === 0) {
-                //do the calculation setup for the first number.
-                sum = calculator.expression[element]
+//     if (calculator.usedAnOperator === true && calculator.defaultDisplay === false) {
+//         //checks if an operator was used once, and if another number was clicked
+//         let run = 0;
+//         calculator.lastExpression = (Number(screenDisplay.textContent))
+//         let sum = null;
+//         for (let element = 0; element < calculator.expression.length; element++) {
+//             //first run
+//             if (element === 0) {
+//                 //do the calculation setup for the first number.
+//                 sum = calculator.expression[element]
                 
-            } else {
-                //not the first iteration
-                //loop just once
-                for (let key in (calculator.expression[element])) {
-                    let value = calculator.expression[element][key]
-                    sum = evaluateIt(key, value, sum)
-                }
-            }
+//             } else {
+//                 //not the first iteration
+//                 //loop just once
+//                 for (let key in (calculator.expression[element])) {
+//                     let value = calculator.expression[element][key]
+//                     sum = evaluateIt(key, value, sum)
 
-            sum = evaluateIt(calculator.currentOperator, calculator.lastExpression,sum)
+//                 }
+//             }
+
+//             sum = evaluateIt(calculator.currentOperator, calculator.lastExpression,sum)
             
-            return sum;
-        }
+//             return sum;
+//         }
     
-    }}
+//     }}
